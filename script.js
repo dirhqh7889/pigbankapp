de21890dd1f28a3dd8480dc96668d54270401647
dirhqh7889 <59160781+dirhqh7889@users.noreply.github.com>
2020-11-26 18:33:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function getInputValue(){
     var valueById = $('#inputId').val();
     var valueById2 = $('#inputId2').val();
+   
     valueById2 = Number(valueById2) + Number(valueById)
-    
-    $("#inputId2").val(valueById2);  
+    $("#inputId2").val(valueById2);
+   
 }
 function getInputValuemino(){
     var valueById = $('#inputId').val();
@@ -21,9 +22,6 @@ function gettarget(){
     
 }
 
-
-    
-
 function calc() {
     var val1 = $("#inputId").val(); // input 값을 가져올 때 사용하는 방식
     var result = val1;
@@ -31,3 +29,37 @@ function calc() {
     alert
 }
 
+function save(){
+    var dataToSave = $('#target_value2').val()
+    var dataToSave2 = $('#inputId2').val();
+    localStorage.setItem("data", dataToSave);
+    localStorage.setItem("dataa", dataToSave2);
+    alert(dataToSave)
+}
+function show(){
+    var dataToShow = localStorage.getItem("data");
+    var dataToShow2 = localStorage.getItem("dataa");
+    $("#target_value2").val(dataToShow);
+    $("#inputId2").val(dataToShow2);
+    
+    }
+function clear(){
+    localStorage.removeItem('data');
+    localStorage.removeItem('dataa');
+
+}
+function vs(){
+    if(Number($('#inputId2').val())>=Number($('#target_value2').val())){
+        var retVal = confirm("목표값 달성!! 축하드립니다. 리셋하시겠습니까?");
+        if(retVal == true){
+            localStorage.clear();
+            show();
+            $('#inputId').val("")
+        }
+        else{
+            alert("리셋하지 않으셨습니다.")
+        }
+
+       
+    }
+}
